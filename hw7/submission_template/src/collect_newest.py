adb9263fc9968b7aac5a3fc50911d5cead8a2304
Douglas Szeto <douglas.szeto@mail.mcgill.ca>
2021-11-15 10:33:50 -0500

    commiting final changes from hw7

--- hw7/submission_template/src/collect_newest.py
+++ hw7/submission_template/src/collect_newest.py
@@ -2,7 +2,7 @@ import argparse, json, requests
 def collect_newest_posts(subreddit_title):
     # set the url to the api of input subreddit
     base_url = f'https://www.reddit.com{subreddit_title}/new.json'
-
+    
     # change the user-agent to mimic a browser and set limit to 100 (default is 25)
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'}
     params = {'limit':'100'}
